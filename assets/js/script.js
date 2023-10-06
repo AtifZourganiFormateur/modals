ec0d02272e10a842dc265d4b7fbd63d549580341
AtifZourganiFormateur <atif.formateur@gmail.com>
2023-10-06 16:43:47 +0200

    kjnfre

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 const btnOpen = document.querySelector('#btnOpen');
 //cible l'element modal qui porte la class modalContainer
 const modal = document.querySelector('.modalContainer');
+//cible l'element btn qui porte la classe closeModal
+const btnClose = document.querySelector('.closeModal');
+
 
 function openModal(){
     //dans l'element modal, utilise la function classlist pour enlever la string displayNone de l'attribus class
